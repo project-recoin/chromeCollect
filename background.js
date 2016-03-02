f9c8d378b59e591d4564542a159feace5375381e
Markus Luczak-Rösch <mail@markus-luczak.de>
2016-03-02 12:47:40 +0000

    popup desktiop notification when new task comes in

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+function notifyMe() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('New Social Computer Task', {
+      //icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: "A new task can be performed.",
+    });
+
+    notification.onclick = function () {
+      window.open("http://sociam.org");      
+    };
+  }
+  setTimeout(notification.close.bind(notification), 1000);
+}
+
 setInterval(function() {
   var xhr = new XMLHttpRequest();
   chrome.storage.sync.get('lastId',function (object) {
@@ -8,6 +29,7 @@ setInterval(function() {
         if(object.lastId == undefined || object.lastId.task_id < resp.task_id){
           chrome.storage.sync.set({'lastId': resp},function (object) {
             chrome.browserAction.setIcon({path: 'success_icon.png'});
+            notifyMe();
             chrome.extension.getBackgroundPage().console.log('saved new latest task');
             chrome.extension.getBackgroundPage().console.log(resp.task_id);
             setTimeout(function(){ chrome.browserAction.setIcon({path: 'icon.png'}); }, 1000);
@@ -18,6 +40,3 @@ setInterval(function() {
     xhr.send();
   });
 }, 10000);
-
-function storeTask(taskObj) {
-}
