bae133fe919784e44abb78679d6f6b3646b28005
Markus Luczak-Rösch <mail@markus-luczak.de>
2016-03-02 12:27:18 +0000

    animate icon when new task comes in

--- background.js
+++ background.js
@@ -7,8 +7,10 @@ setInterval(function() {
         var resp = JSON.parse(xhr.responseText);
         if(object.lastId == undefined || object.lastId.task_id < resp.task_id){
           chrome.storage.sync.set({'lastId': resp},function (object) {
+            chrome.browserAction.setIcon({path: 'success_icon.png'});
             chrome.extension.getBackgroundPage().console.log('saved new latest task');
             chrome.extension.getBackgroundPage().console.log(resp.task_id);
+            setTimeout(function(){ chrome.browserAction.setIcon({path: 'icon.png'}); }, 1000);
           });
         }
       }
